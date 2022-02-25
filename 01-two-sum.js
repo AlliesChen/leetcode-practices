22e998c1456853c6996b9b268440e195aabdae30
YPChen <cyphuaenng@pm.me>
2022-02-25 19:06:32 +0800

    Update content

--- 01-two-sum.js
+++ 01-two-sum.js
@@ -1,3 +1,8 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
 var twoSum = function (nums, target) {
   const map = {};
   for (let i = 0; i < nums.length; i++) {
