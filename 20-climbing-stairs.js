7a466afe79e8dfcb3fc5a32a465d58f93ee99bbc
YPChen <cyphuaenng@pm.me>
2022-02-26 01:34:30 +0800

    Add Q20

--- 20-climbing-stairs.js
+++ 20-climbing-stairs.js
@@ -2,6 +2,20 @@
  * @param {number} n
  * @return {number}
  */
-var climbStairs = function(n) {
-    
+var climbStairs = function (n) {
+  //base stair = 0
+  let stair = 2;
+  const sum = [1, 2];
+  while (stair < n) {
+    sum[2] = sum[0] + sum[1];
+    sum[0] = sum[1];
+    sum[1] = sum[2];
+    stair += 1;
+  }
+  return sum[2] ?? n;
 };
+
+//This implement the concept of buttom-up
+//https://leetcode.com/problems/climbing-stairs/discuss/1792723/Python-or-In-Depth-Walkthrough-+-Explanation-or-DP-Top-Down-+-Bottom-Up
+//A better solution is using math
+//https://akuli.github.io/math-tutorial/fib.html
