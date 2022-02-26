1a111466acb05511d108612c63d894936e0dbf3c
YPChen <cyphuaenng@pm.me>
2022-02-26 11:47:39 +0800

    fix typo

--- 20-climbing-stairs.js
+++ 20-climbing-stairs.js
@@ -15,7 +15,7 @@ var climbStairs = function (n) {
   return sum[2] ?? n;
 };
 
-//This implement the concept of buttom-up
+//This implements the concept of buttom-up
 //https://leetcode.com/problems/climbing-stairs/discuss/1792723/Python-or-In-Depth-Walkthrough-+-Explanation-or-DP-Top-Down-+-Bottom-Up
 //A better solution is using math
 //https://akuli.github.io/math-tutorial/fib.html
