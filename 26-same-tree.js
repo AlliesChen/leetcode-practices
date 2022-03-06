617356e0f2a9d47d573836c23cd4901ae101c537
YPChen <cyphuaenng@pm.me>
2022-03-06 18:26:46 +0800

    Add file

---
+++ 26-same-tree.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function (p, q) {
+  // Both nodes end
+  if (p === null && q === null) return true;
+  // Both nodes have value
+  if (!p || !q) return false;
+  // Each node has the same value
+  if (p.val !== q.val) return false;
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
